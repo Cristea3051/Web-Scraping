e3487f040ec6245a6e0f2b18617b5acee9358849
Cristea3051 <cristeavictor4@gmail.com>
2025-05-04 16:21:33 +0300

    It was activated headless and maximized window mode

--- src/main/java/com/parser/Main.java
+++ src/main/java/com/parser/Main.java
@@ -6,14 +6,18 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50).setArgs(java.util.Arrays.asList("--start-maximized")));
+            List<String> arguments = new ArrayList<>();
+            arguments.add("--start-maximized");
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setArgs(arguments));
             Page page = browser.newPage();
             page.navigate("https://dprp.gov.ro/web/rezultate-sesiune-de-finantare-2020/");
-            page.waitForTimeout(60_000);
+            page.waitForTimeout(10_000);
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
         }
     }
